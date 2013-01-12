ebf315a06e8b1a4ae9596a00387c84ca6f39ca23
unclejamal <unclejamal@unclejamal-pc.(none)>
2013-01-12 16:52:17 +0100

    promoting view to src

--- penny-android/src/main/java/com/pduda/penny/view/android/BrowseTransactionsActivity.java
+++ penny-android/src/main/java/com/pduda/penny/view/android/BrowseTransactionsActivity.java
@@ -3,6 +3,7 @@ package com.pduda.penny.view.android;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import com.pduda.penny.domain.mvp.BrowseTransactionsView;
 import com.pduda.penny.toolkit.ProgrammerMistake;
 
 public class BrowseTransactionsActivity extends Activity implements BrowseTransactionsView {
