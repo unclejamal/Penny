5a96a2f307c3faa73bbd70498f6611f4c6298309
unclejamal <unclejamal@gmail.com>
2013-01-27 22:15:30 +0100

    380

--- penny-domain/src/test/java/com/pduda/penny/domain/view/DateCsvFormat.java
+++ penny-domain/src/test/java/com/pduda/penny/domain/view/DateCsvFormat.java
@@ -1,11 +1,15 @@
 package com.pduda.penny.domain.view;
 
 import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public class DateCsvFormat implements CsvFormat<LocalDate> {
 
+    public static final DateTimeFormatter ISO_FORMAT = DateTimeFormat.forPattern("yyyy-MM-dd");
+
     @Override
     public String format(LocalDate date) {
-        return "2012-11-14";
+        return ISO_FORMAT.print(date);
     }
 }
